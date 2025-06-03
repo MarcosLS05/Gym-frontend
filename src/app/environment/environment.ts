4d6d509f831b7899dff2f6f5566efbf4cc100031
MarcosLS05 <marcoscurro1234@gmail.com>
2025-06-03 21:35:46 +0200

    cambios ok

--- src/app/environment/environment.ts
+++ src/app/environment/environment.ts
@@ -4,7 +4,7 @@ export const environment = {
   production: false,
 };
 
-export const serverURL: string = 'http://localhost:8085';
+export const serverURL: string = 'https://gimnasio-db.puntaserver.com';
 
 export const httpOptions = {
   headers: new HttpHeaders({
